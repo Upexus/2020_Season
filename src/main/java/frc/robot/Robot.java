dc3da7b215abcf4632a7784ac172b5256a213fe2
ZacheryCalahan <45713103+ZacheryCalahan@users.noreply.github.com>
2020-02-18 16:46:41 -0500

    Organization

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,16 @@
 package frc.robot;
-
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+//Drive
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+//Auto
 import edu.wpi.first.wpilibj.Timer;
+
+//Output
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+//Input
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 
@@ -18,32 +24,29 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.ctre.phoenix.motorcontrol.TalonFXSensorCollection;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANEncoder;
 
+//Pnuematics 
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Compressor;
+
 
 public class Robot extends TimedRobot {
-  //Vars used by the whole class, put here to only init once.
+  //Variables
   boolean search = false;
-  
-  //Limelight
-  private boolean LimelightHasValidTarget = false;
-  private double LimelightDriveCommand = 0.0;
-  private double LimelightSteerCommand = 0.0;
 
   //Motor Controllers
-  private WPI_TalonFX topRightDrivey, topLeftDrivey, bottomRightDrivey, bottomLeftDrivey; 
-  private WPI_TalonFX shooterAlpha, shooterBeta; 
+  private WPI_TalonFX frontLeft, frontRight, rearLeft, rearRight;
+  private WPI_TalonFX leftShooter, rightShooter;
   private CANSparkMax turret;
-  private CANEncoder turretSensor;
-  private SpeedControllerGroup leftDrivey, rightDrivey, shooter;
   private TalonFXSensorCollection shooterSensor;
-  private DifferentialDrive drivey;
+  private DifferentialDrive drive;
+  private SpeedControllerGroup leftDrivey, rightDrivey, shooters;
 
   //Misc
   private Timer time;
-  private Joystick logitech;
+  private Joystick driver, mechanic;
 
   //NetworkTable
   private NetworkTable table;
@@ -51,34 +54,40 @@ public class Robot extends TimedRobot {
   private NetworkTableEntry ty;
   private NetworkTableEntry ta;
 
+  //Sensors
+  private CANEncoder turretSensor;
+
   /* Notes:
   https://docs.limelightvision.io/en/latest/cs_aiming.html AIMING
   https://docs.limelightvision.io/en/latest/cs_estimating_distance.html DISTANCE ESTIMATION
-  https://docs.limelightvision.io/en/latest/getting_started.html#wiring BASIC PROGRAMMING AND WIRING AND SHIT
+  https://docs.limelightvision.io/en/latest/getting_started.html#wiring BASIC PROGRAMMING AND WIRING
   */
 
   @Override
   public void robotInit() {
     //Auto Stuff
     time     = new Timer();
-    logitech = new Joystick(0);
+    driver = new Joystick(0);
+    mechanic = new Joystick(1);
 
     //Mechanisms
-    shooterAlpha = new WPI_TalonFX(9);
-    shooterBeta  = new WPI_TalonFX(10);
-    shooter      = new SpeedControllerGroup(shooterAlpha, shooterBeta);
-    shooter        .setInverted(true);
-    turret       = new CANSparkMax(11, MotorType.kBrushless);
+    leftShooter = new WPI_TalonFX(9);
+    rightShooter = new WPI_TalonFX(10);
+    // Change which shooter is inverted in the case that both rotate in the wrong direction.
+    leftShooter.setInverted(true);
+    shooters = new SpeedControllerGroup(leftShooter, rightShooter);
+
+    turret = new CANSparkMax(11, MotorType.kBrushless);
     turretSensor = new CANEncoder(turret);
 
     //Drive
-    topLeftDrivey    = new WPI_TalonFX(7);
-    topRightDrivey   = new WPI_TalonFX(5);
-    bottomLeftDrivey = new WPI_TalonFX(8);
-    bottomLeftDrivey = new WPI_TalonFX(6);
-    leftDrivey  = new SpeedControllerGroup(topLeftDrivey, bottomLeftDrivey);
-    rightDrivey = new SpeedControllerGroup(topRightDrivey, bottomRightDrivey);
-    drivey = new DifferentialDrive(leftDrivey, rightDrivey);
+    frontLeft    = new WPI_TalonFX(5);
+    frontRight   = new WPI_TalonFX(6);
+    rearLeft = new WPI_TalonFX(7);
+    rearRight = new WPI_TalonFX(8);
+    leftDrivey  = new SpeedControllerGroup(frontLeft, rearLeft);
+    rightDrivey = new SpeedControllerGroup(frontRight, rearRight);
+    drive = new DifferentialDrive(leftDrivey, rightDrivey);
 
     //NetworkTable stuffs
     table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -89,15 +98,7 @@ public class Robot extends TimedRobot {
   
   @Override
   public void robotPeriodic() {
-    //read values periodically
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
 
-    //post to smart dashboard periodically for debugging purposes
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);
   }
 
   @Override
@@ -107,28 +108,31 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousPeriodic() {
-
+    //read values periodically
+    //double x = tx.getDouble(0.0);
+    //double y = ty.getDouble(0.0);
+    //double area = ta.getDouble(0.0);
   }
 
   @Override
   public void teleopPeriodic() {
     //Drive
-    drivey.arcadeDrive(logitech.getRawAxis(1), logitech.getRawAxis(4));
+    drive.arcadeDrive(driver.getRawAxis(1), driver.getRawAxis(4));
 
     //Shooter
-    if (logitech.getRawButton(8)) {
-      shooter.set(0.5);
+    if (driver.getRawButton(8)) {
+      shooters.set(1);
     } else {
-      shooter.set(0.0);
+      shooters.set(0.0);
     }
 
     //Turret
-    if (logitech.getRawButtonPressed(1)) {
+    if (driver.getRawButtonPressed(1)) {
       search = !search;
     } 
 
     if (search) {
-      limelightChecker();
+      limelight();
     }
 
     //have thing for turret for possible manual control
@@ -139,7 +143,7 @@ public class Robot extends TimedRobot {
 
   }
 
-  public void limelightChecker() {
+  public void limelight() {
     double tx = table.getEntry("tx").getDouble(0.0); //x axis offset
     double tv = table.getEntry("tv").getDouble(0.0); //whether or not we see the goal
     double kp = -0.1;
@@ -164,9 +168,4 @@ public class Robot extends TimedRobot {
       turret.set(turret_adjust);
     }
   }
-
-  public void seeker() {
-
-  }
-
 }
